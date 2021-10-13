eb9fae577cf315b433112369d256b60391c836bb
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-10-13 14:00:19 +0530

    As a User Need to Follow Pre-defined Mobile Format

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Day21_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {  UC4 Email = new UC4();
+            Console.WriteLine("Enter Country code in start  eg(91) for India");
+            Console.WriteLine("Enter the Phone Number");
+
+            name4 = Console.ReadLine();
+
+            result4 = Email.PhonenumberCheck(name4);
+            Console.WriteLine(result4);
+
+          }
+     }
+}
